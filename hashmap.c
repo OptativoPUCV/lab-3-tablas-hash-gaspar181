cd54aed114ce828ee973d8655ac9954e080e48a8
Your Name <you@example.com>
2025-04-18 16:39:58 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -53,12 +53,12 @@ void enlarge(HashMap * map) {
 
 HashMap * createMap(long capacity) {
     HashMap *nuevoMapa = (HashMap *)malloc(sizeof(HashMap));
-    if(nuevoMapa == NULL) return EXIT_FAILURE;
+    if(nuevoMapa == NULL) return NULL;
 
     nuevoMapa->buckets = (Pair **)malloc(capacity * sizeof(Pair *));
     if(nuevoMapa->buckets == NULL){
         free(nuevoMapa);
-        return EXIT_FAILURE;
+        return NULL;
     }
 
     for(long i = 0; i < capacity; i++){
